b4236c3c140376911b416558aef07af31e1d8fa4
professorjoaomoreira <professorjoaomoreira@gmail.com>
2022-07-09 18:36:58 -0300

    enviando até a aula 05

---
+++ desafio 005 - mostrar um numero depois seu sucessor e antecessor.py
@@ -0,0 +1,4 @@
+numero = int(input('digite um número: '))
+ant = (numero - 1)
+suc = (numero +1)
+print (' O número digitado é: {} o número antecessor é: {} e o número posterior é: {}'.format(numero, ant, suc))
